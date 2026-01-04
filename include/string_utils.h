f393277d5680d4db37b3c6b00d385697621b8d6f
goaki67 <vh4qt6i26f@tuta.com>
2026-01-04 21:49:07 +0200

    Added functions to string_utils

--- include/string_utils.h
+++ include/string_utils.h
@@ -2,6 +2,24 @@
 #define STRING_UTILS_H
 
 #include <stddef.h>
+#include <stdlib.h>
+
+typedef struct {
+  size_t length;
+  size_t allocated;
+  char *data;
+} string_t;
+
+// gets a char* and makes a string_t
+int string_init(string_t *, char *);
+// free
+void string_destroy(string_t *);
+// addes the char* to the end of string_t
+int string_append(string_t *, char *);
+int string_append_s(string_t *, string_t *);
+// check if string_t* strats with the second string_t*
+bool string_starts_with(string_t *, char *);
+bool string_starts_with_s(string_t *, string_t *);
 
 [[nodiscard]]
 char **str_split(char *input, char delimiter);
